0b36370691bc4e0910a7c7e234656d2f667d4180
Wilfried AGO <wilfriedago@pm.me>
2024-07-30 11:32:29 +0100

    chore: add necessary configuration files and update dependencies

---
+++ lint-staged.config.mjs
@@ -0,0 +1,11 @@
+import { relative } from 'node:path'
+
+const buildEslintCommand = (filenames) => `next lint --fix --strict --max-warnings 0 --report-unused-disable-directives-severity warn --file ${filenames.map((f) => relative(process.cwd(), f)).join(' --file ')}`
+
+const config = {
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "**/*.ts?(x)": ["tsc --noEmit --pretty"],
+  "*.{json,yaml,md}": ["prettier --check"]
+}
+
+export default config
---
+++ reset.d.ts
@@ -0,0 +1,2 @@
+// Do not add any other lines of code to this file!
+/// <reference types="@total-typescript/ts-reset" />

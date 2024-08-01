c0f809a3b3f7ca28ae43b43972d4c42930788a1f
Wilfried AGO <wilfriedago@pm.me>
2024-08-01 21:50:04 +0100

    chore: update lint-staged and tsc commands
    Update lint-staged.config.mjs to fix the buildEslintCommand function and add the buildTscCommand function. The buildEslintCommand now correctly handles filenames with spaces, and the buildTscCommand reads the tsconfig.json file to generate the tsc command with compiler options.
    
    These changes improve the linting and type-checking processes for JavaScript and TypeScript files.

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,11 +1,22 @@
-import { relative } from "node:path"
+import fs from "node:fs"
 
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --strict --max-warnings 0 --report-unused-disable-directives-severity warn --file ${filenames.map((f) => relative(process.cwd(), f)).join(" --file ")}`
+  `next lint --fix --strict --max-warnings 0 --report-unused-disable-directives-severity warn --file ${filenames.join(" --file ")}`
+
+const buildTscCommand = (filenames) => {
+  const tsconfig = JSON.parse(fs.readFileSync("tsconfig.json", "utf8"))
+
+  const options = Object.entries(tsconfig.compilerOptions)
+    .filter(([key]) => !["paths", "plugins", "incremental"].includes(key))
+    .map(([key, value]) => (value === true ? `--${key}` : `--${key} ${value}`))
+    .join(" ")
+
+  return `tsc --pretty ${options} ${filenames.join(" ")}`
+}
 
 const config = {
   "*.{js,jsx,ts,tsx}": [buildEslintCommand],
-  "**/*.ts?(x)": ["tsc --noEmit --pretty"],
+  "**/*.ts?(x)": [buildTscCommand],
   "*.{json,yaml,md}": ["prettier --check"]
 }
 

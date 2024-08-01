f0b8ce2e7030ddfe7324f630409fd9564e3086ba
Wilfried AGO <wilfriedago@pm.me>
2024-08-01 19:25:20 +0100

    chore: update commitlint.config.mjs
    This commit updates the commitlint.config.mjs file by removing the custom rules section. The custom
    rules were causing issues and are no longer necessary.
    
    This change ensures that commitlint uses the @commitlint/config-conventional configuration without
    any overrides or customizations.

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,19 +1,5 @@
 const config = {
-  /*
-   * Resolve and load @commitlint/config-conventional from node_modules.
-   * Referenced packages must be installed
-   */
-  extends: ["@commitlint/config-conventional"],
-  /*
-   * Any rules defined here will override rules from @commitlint/config-conventional
-   */
-  rules: {
-    "type-enum": [2, "always", ["foo"]]
-  },
-  /*
-   * Whether commitlint uses the default ignore rules, see the description above.
-   */
-  defaultIgnores: true
+  extends: ["@commitlint/config-conventional"]
 }
 
 export default config

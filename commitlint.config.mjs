8815d4c8cb860f37d2af20270bfef2bfcac74df7
Wilfried AGO <wilfriedago@pm.me>
2024-08-01 21:19:09 +0100

    chore: enforce body-max-line-length rule in commit messages
    Update commitlint.config.mjs to enforce a maximum line length of 150 characters for commit message bodies.
    
    This change ensures commit messages remain readable and maintainable by adhering to a consistent format.

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,5 +1,24 @@
+/**
+ * Rules always have a severity.
+ * Severity indicates what to do if the rule is found to be broken.
+ *
+ * 0 - Disable this rule
+ *
+ * 1 - Warn for violations
+ *
+ * 2 - Error for violations
+ */
+const RuleConfigSeverity = {
+  Disabled: 0,
+  Warning: 1,
+  Error: 2
+}
+
 const config = {
-  extends: ["@commitlint/config-conventional"]
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "body-max-line-length": [RuleConfigSeverity.Error, "always", 150]
+  }
 }
 
 export default config
